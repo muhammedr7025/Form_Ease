410f9da1f766777ea900384128220d61c69051be
muhammedr7025 <muhammedr7025@gmail.com>
2023-04-07 09:33:16 +0530

    dashboard completed

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
   let no_of_qn = document.getElementById('no_of_qn');
   let most_corrected_answer = document.getElementById('most_corrected_answer');
   let most_wrong_answer=document.getElementById('most_wrong_answer');
   let total_no_attendes=document.getElementById('total_no_attendes');
+  let qn_container = document.getElementById("qn_container");
   let result = []
   let highest = 0
   let lowest=0
@@ -44,6 +44,51 @@ document.addEventListener('DOMContentLoaded', () => {
         most_wrong_answer.innerHTML=qnnolowest
         most_corrected_answer.innerHTML=qnnohighest
         total_no_attendes.innerHTML=parseInt(result[0].no_of_correct)+parseInt(result[0].no_of_wrong)
+        for (let i = 0; i <result.length; i++) {
+            const div = document.createElement("div");
+            
+            div.classList.add("question");
+            div.innerHTML = `
+            <h4>${result[i].numb}. ${result[i].question}</h4>
+            
+            <h4>ans. ${getAnswer(result[i].answer,i)}</h4>
+            <div class="bar-container">
+              <div class="bar">
+                <div class="green-bar" style="width:${getPercentage("correct",i)}%"></div>
+                <p>${getPercentage("correct",i)}%</p>
+              </div>
+              <div class="bar">
+                <div class="red-bar" style="width:${getPercentage("wrong",i)}%"></div>
+                  <p>${getPercentage("wrong",i)}%</p>
+                </div>
+            </div>
+            `;
+            qn_container.appendChild(div);
+        }
       });
     });
-});
+
+    function getAnswer(answer,i){
+      let ans = ""
+      if(answer=='a'||answer=='A'){
+        ans=result[i].options[0]
+      }else if(answer=='b'||answer=='B'){
+         ans=result[i].options[1]
+      }else if(answer=='c'||answer=='C'){
+         ans=result[i].options[2]
+      }else if(answer=='d'||answer=='D'){
+         ans=result[i].options[3]
+      }
+      return ans
+    }
+    function getPercentage(option,i){
+        let total=parseInt(result[i].no_of_wrong)+parseInt(result[i].no_of_correct)
+        let percentage = 0
+        if(option==="correct"){
+            percentage=(parseInt(result[i].no_of_correct)*100)/total
+        }else if(option==="wrong"){   
+            percentage=(parseInt(result[i].no_of_wrong)*100)/total
+        }
+        return parseInt(percentage)
+    }
+   
